ba9316841c9bc745d79466b4c3710e8a65cec2fd
yuxinbaiwin <37678344+yuxinbaiwin@users.noreply.github.com>
2018-06-17 22:48:44 +0800

    Create main.py

---
+++ hw01/main.py
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<html>
+<body>
+
+<table width="600" border="1">
+<tr>
+<td colspan="3" style="background-color:#99bbbb;">
+<h1>Main Title of Web Page</h1>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="background-color:#ffff99;width:200px;text-align:top;">
+<b>Menu</b><br />
+HTML<br />
+CSS<br />
+JavaScript
+</td>
+<td style="background-color:#EEEEEE;height:200px;width:200px;text-align:top;">
+Content goes here</td>
+<td>愿你总能到达希望的终点</td>
+</tr>
+
+<tr>
+<td colspan="3" style="background-color:#99bbbb;text-align:center;">
+Copyright W3School.com.cn</td>
+</tr>
+</table>
+
+</body>
+</html>
